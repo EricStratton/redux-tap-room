048258f166c375d3a7e173a3b6ff336c2b308004
Eric Stratton <strattonericj@gmail.com>
2021-02-12 10:26:40 -0800

    update control file to handle editing existing keg entries

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -10,7 +10,8 @@ class KegControl extends React.Component {
     this.state = {
       formVisibleOnPage: false,
       mainKegList: [],
-      selectedKeg: null
+      selectedKeg: null,
+      editing: false
     };
   }
 
@@ -27,11 +28,27 @@ class KegControl extends React.Component {
     this.setState({ selectedKeg: selectedKeg })
   }
 
+  handleEditKegInList = (kegToEdit) => {
+    const editedMainKegList = this.state.mainKegList
+      .filter(keg => keg.id !== this.state.selectedKeg.id)
+      .concat(kegToEdit);
+    this.setState({
+      mainKegList: editedMainKegList,
+      editing: false,
+      selectedKeg: null
+    });
+  }
+
+  handleEditClick = () => {
+    this.setState({ editing: true })
+  }
+
   handleClick = () => {
     if (this.state.selectedKeg != null) {
       this.setState({
         formVisibleOnPage: false,
-        selectedKeg: null
+        selectedKeg: null,
+        editing: false
       });
     } else {
       this.setState(prevState => ({
@@ -43,7 +60,9 @@ class KegControl extends React.Component {
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.selectedKeg != null) {
+      if (this.state.editing) {
+      currentlyVisibleState = <EditKegForm keg={ this.state.selectedKeg } />
+    } else if (this.state.selectedKeg != null) {
       currentlyVisibleState = <KegDetail keg={ this.state.selectedKeg }/>
       buttonText = "Return to Keg List";
     } else if (this.state.formVisibleOnPage) {
