99d78dd7b7fa700916ecaf889497ed246647c8ec
Eric Stratton <strattonericj@gmail.com>
2021-02-12 09:37:50 -0800

    build out keg list file

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Keg from './Keg';
+import PropTypes from 'prop-types';
 
-function KegList() {
+function KegList(props) {
   return (
     <>
-      {/* .map through all kegs */}
+      {props.kegList.map((keg) => 
+        <Keg
+          whenItemClicked = { props.onKegSelection }
+          name={ keg.name }
+          brand={ keg.brand }
+          price={ keg.price }
+          abv={ keg.abv }
+          stock={ keg.stock }
+          key={ keg.id }
+          id={ keg.id } />
+      )}
     </>
   );
 }
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
+};
+
+export default KegList;
