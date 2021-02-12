91ecd6f26feda8aaeecfb1db3a96243e081f0b60
Eric Stratton <strattonericj@gmail.com>
2021-02-12 10:34:00 -0800

    add built out edit keg form

---
+++ src/components/EditKegForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReusableFrom from './ReusableForm';
+
+function EditKegForm(props) {
+  const { keg } = props;
+
+  function handleEditKegFormSubmission(event) {
+    event.preventDefault();
+    props.onEditKeg({ name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, abv: event.target.abv.value, stock: event.target.stock.value, id: keg.id })
+  }
+  return (
+    <>
+      <ReusableFrom
+        formSubmissionHandler={handleEditKegFormSubmission}
+        buttonText="Update Keg" />
+    </>
+  );
+}
+
+EditKegForm.propTypes = {
+  keg: PropTypes.object,
+  onEditKeg: PropTypes.func
+};
+
+export default EditKegForm;
