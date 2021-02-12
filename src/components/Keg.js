66ac5e6e779532b1f264996cf15383942990d817
Eric Stratton <strattonericj@gmail.com>
2021-02-12 09:39:31 -0800

    build out keg file

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Keg() {
   return (
     <>
+      <div onClick = {() => props.whenKegClicked(props.id)}>
+        <h3>{ props.name }</h3>
+      </div>
     </>
   );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired
 }
 
 export default Keg;
