86b64ec3b1eb1a7069b89b9589205faf32ee2e31
Eric Stratton <strattonericj@gmail.com>
2021-02-12 15:19:41 -0800

    change font in keg detail file

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function KegDetail(props) {
+  const myStyledKegDetail = {
+    paddingLeft: '10px'
+  };
+
   const { keg, onClickingDelete, onClickingPour } = props; // Object Destructuring //
 
   let sellButton; 
   let poursLeft;
   if(keg.stock !== 0) {// Handle hiding sell button and displaying 'Keg Empty' warning when stock reaches 0.
-    poursLeft = <h4>{ keg.stock }</h4>;
+    poursLeft = <h4>Pours: { keg.stock }</h4>;
     sellButton = <button onClick={ () => onClickingPour() }>Sell</button>
   } else {
       poursLeft = <h4><strong>Keg is Empty</strong></h4>;
@@ -15,13 +19,17 @@ function KegDetail(props) {
 
   return (
     <>
-      <h3>Keg Details</h3>
-      <hr />
-      <h3>{ keg.name } - { keg.brand }</h3>
-      <h4>Price: ${ keg.price }/pint</h4>
-      <h4>ABV: { keg.abv }%</h4>
-      { poursLeft }
-      { sellButton }
+      <div style={ myStyledKegDetail }> 
+        <h3>Keg Details</h3>
+      </div>
+        <hr />
+      <div style={ myStyledKegDetail }>
+        <h3>{ keg.name } - { keg.brand }</h3>
+        <h4>Price: ${ keg.price }/pint</h4>
+        <h4>ABV: { keg.abv }%</h4>
+        { poursLeft }
+      </div>
+        { sellButton }
       <button onClick={ props.onClickingEdit }>Update Keg</button>
       <button onClick={ () => onClickingDelete(keg.id) }>Remove Keg</button>
     </>
