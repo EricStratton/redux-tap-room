b12c6789feae3158d8025fc92460e82c6eb2d4fd
Eric Stratton <strattonericj@gmail.com>
2021-02-18 09:25:41 -0800

    update newKegForm to parseInt values that are meant to be numbers

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -7,7 +7,13 @@ function NewKegForm(props) {
 
   function handleNewKegFormSubmission(event) {
     event.preventDefault();
-    props.onNewKegCreation({ name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, abv: event.target.abv.value, stock: event.target.stock.value, id: v4() });
+    props.onNewKegCreation({ 
+      name: event.target.name.value, 
+      brand: event.target.brand.value, 
+      price: parseInt(event.target.price.value), 
+      abv: parseInt(event.target.abv.value), 
+      stock: parseInt(event.target.stock.value), 
+      id: v4() });
   }
 
   return (
