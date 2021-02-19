466954c2b648fd8b0fbe73cbb996134c89ef88a3
Eric Stratton <strattonericj@gmail.com>
2021-02-19 10:47:07 -0800

    add tests for return default of each slice of state

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from './../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from './../../reducers/form-visible-reducer';
+import kegListReducer from './../../reducers/keg-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
   
@@ -7,5 +12,13 @@ describe("rootReducer", () => {
       mainKegList: {},
       formVisibleOnPage: false
     });
-  })
+  });
+
+  test('Check inital state of kegListReducer matches root reducer', () => {
+    expect(store.getState().mainKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test('Check inital state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
 });
