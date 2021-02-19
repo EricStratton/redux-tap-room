21ec20489d8e94436bf3ba166af31df5c29bb807
Eric Stratton <strattonericj@gmail.com>
2021-02-19 13:55:41 -0800

    working on figuring out how to build this reducer

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -1,8 +1,38 @@
 import selectedKegReducer from './../../reducers/selected-keg-reducer';
+import * as c from './../../actions/ActionTypes';
 
 describe("selectedKegReducer", () => {
 
+  const currentState = {
+    1: { name: 'Super Saison',
+    brand: 'Super Brews',
+    price: 8,
+    abv: 7,
+    stock: 124,
+    id: 1 },
+    2: { name: 'Serious Stout',
+    brand: 'Moose Beers',
+    price: 10,
+    abv: 12,
+    stock: 124,
+    id: 2 }
+  }
+
   test('Should return default state if no action passed into reducer', () => {
     expect(selectedKegReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should select correct keg by ID', () => {
+    action = {
+      
+    };
+    expect(selectedKegReducer({}[1], { type: action })).toEqual({
+      1: { name: 'Super Saison',
+    brand: 'Super Brews',
+    price: 8,
+    abv: 7,
+    stock: 124,
+    id: 1 }
+    })
   });
 });
