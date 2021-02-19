2f64af2bdaaf0ce593cb7d35811021b92a3c842f
Eric Stratton <strattonericj@gmail.com>
2021-02-19 11:34:26 -0800

    update control file to use action creators

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -5,6 +5,7 @@ import KegDetail from './KegDetail';
 import EditKegForm from './EditKegForm';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import * as a from './../actions';
 
 class KegControl extends React.Component {
 
@@ -18,21 +19,10 @@ class KegControl extends React.Component {
 
   handleAddingNewKegToList = (newKeg) => {
     const { dispatch } = this.props;
-    const { id, name, brand, price, abv, stock } = newKeg;
-    const action = {
-      type: 'ADD_KEG',
-      id,
-      name,
-      brand,
-      price,
-      stock,
-      abv
-    }
+    const action = a.addKeg(newKeg);
     dispatch(action);
-    const action2 = {
-      type: 'TOGGLE_FORM'
-    }
-    dispatch(action);
+    const action2 = a.toggleForm();
+    dispatch(action2);
   }
 
   handleChangingSelectedKeg = (id) => {
@@ -42,16 +32,7 @@ class KegControl extends React.Component {
 
   handleEditKegInList = (kegToEdit) => {
     const { dispatch } = this.props;
-    const { id, name, brand, price, abv, stock } = kegToEdit;
-    const action = {
-      type: 'ADD_KEG',
-      id,
-      name,
-      brand,
-      price,
-      stock,
-      abv
-    }
+    const action = a.addKeg(kegToEdit);
     dispatch(action); 
     this.setState({
       editing: false,
@@ -61,10 +42,7 @@ class KegControl extends React.Component {
 
   handleDeletingKeg = (id) => {
     const { dispatch } = this.props;
-    const action = {
-      type: 'DELETE_KEG',
-      id
-    }
+    const action = a.deleteKeg(id);
     dispatch(action);
     this.setState({
       selectedKeg: null
@@ -102,9 +80,7 @@ class KegControl extends React.Component {
       });
     } else {
       const { dispatch } = this.props;
-      const action = {
-        type: 'TOGGLE_FORM'
-      }
+      const action = a.toggleForm();
       dispatch(action);
       };
     }
