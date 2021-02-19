2f78b2329db101f9fd6d6db872641a1ddead7ead
Eric Stratton <strattonericj@gmail.com>
2021-02-19 11:43:55 -0800

    add pourFromKeg action creator and pass its test

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,3 +19,16 @@ export const deleteKeg = id => ({
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
+
+export const pourFromKeg = (keg) => {
+  const { name, brand, price, abv, stock, id } = keg;
+  return {
+    type: 'ADD_KEG',
+    name,
+    brand,
+    price,
+    abv,
+    stock: stock - 1,
+    id
+  }
+}
