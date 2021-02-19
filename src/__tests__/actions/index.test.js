0675442b6f8554c5b10a59c1e3b71d63a5bd6f55
Eric Stratton <strattonericj@gmail.com>
2021-02-19 11:39:43 -0800

    add pourFromKeg action creator test and fail

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -20,6 +20,25 @@ describe('tap room actions', () => {
     });
   });
 
+  it('pourFromKeg should create ADD_KEG action but stock by one', () => {
+    expect(actions.pourFromKeg({
+      name: 'Purrfect Pilsner',
+      brand: 'Bear-ery',
+      price: 6,
+      abv: 5,
+      stock: 124,
+      id: 1
+    })).toEqual({
+      type: 'ADD_KEG',
+      name: 'Purrfect Pilsner',
+      brand: 'Bear-ery',
+      price: 6,
+      abv: 5,
+      stock: 123,
+      id: 1
+    })
+  })
+
   it('deleteKeg should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1)).toEqual({
       type: 'DELETE_KEG',
@@ -32,5 +51,5 @@ describe('tap room actions', () => {
       type: 'TOGGLE_FORM'
     });
   });
-  
+
 });
