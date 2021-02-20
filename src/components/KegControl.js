912ed7aa32f21d80bc37af0e5b5576a2134e51d5
Eric Stratton <strattonericj@gmail.com>
2021-02-19 17:01:15 -0800

    begin working on changing selectedKeg local state references to Redux

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -12,7 +12,7 @@ class KegControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedKeg: null,
+      // selectedKeg: null,
       editing: false
     };
   }
@@ -25,9 +25,11 @@ class KegControl extends React.Component {
     dispatch(action2);
   }
 
-  handleChangingSelectedKeg = (id) => {
+  handleChangingSelectedKeg = (id) => { // Am I properly handling this selected keg? // Feels like yes but details view still not working //
     const selectedKeg = this.props.mainKegList[id];
-    this.setState({ selectedKeg: selectedKeg }) // would become a.selectKeg(selectedKeg);
+    const { dispatch } = this.props;
+    const action = a.selectKeg(selectedKeg);
+    dispatch(action);
   }
 
   handleEditKegInList = (kegToEdit) => {
@@ -64,9 +66,12 @@ class KegControl extends React.Component {
   }
 
   handleClick = () => {
-    if (this.state.selectedKeg != null) {
+    if (this.props.selectedKeg != null) {
+      // const { dispatch } = this.props; // Unsure at best about this. //
+      // const action = a.selectKeg();
+      // dispatch(action);
       this.setState({
-        selectedKeg: null,
+        // selectedKeg: null,
         editing: false
       });
     } else {
@@ -83,8 +88,9 @@ class KegControl extends React.Component {
       if (this.state.editing) {
       currentlyVisibleState = <EditKegForm keg={ this.state.selectedKeg } onEditKeg={ this.handleEditKegInList } />
       buttonText = "Return to Keg List";
-    } else if (this.state.selectedKeg != null) {
-      currentlyVisibleState = <KegDetail keg={ this.state.selectedKeg } onClickingPour={ this.handlePoursInKeg } onClickingEdit={ this.handleEditClick } onClickingDelete={ this.handleDeletingKeg }/>
+    } else if (this.props.selectedKeg != null) {
+      // Details view is not currently working. // Related to how keg is being passed down as a prop? // Related to how the handleClick method is refering to selectedKeg action? //
+      currentlyVisibleState = <KegDetail keg={ this.props.selectedKeg } onClickingPour={ this.handlePoursInKeg } onClickingEdit={ this.handleEditClick } onClickingDelete={ this.handleDeletingKeg }/>
       buttonText = "Return to Keg List";
     } else if (this.props.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm onNewKegCreation={ this.handleAddingNewKegToList } />
@@ -104,13 +110,15 @@ class KegControl extends React.Component {
 
 KegControl.propTypes = {
   mainKegList: PropTypes.object,
-  formVisibleOnPage: PropTypes.bool
+  formVisibleOnPage: PropTypes.bool,
+  selectedKeg: PropTypes.object
 }
 
 const mapStateToProps = state => {
   return {
     mainKegList: state.mainKegList,
-    formVisibleOnPage: state.formVisibleOnPage
+    formVisibleOnPage: state.formVisibleOnPage,
+    selectedKeg: state.selectKeg
   }
 }
 
