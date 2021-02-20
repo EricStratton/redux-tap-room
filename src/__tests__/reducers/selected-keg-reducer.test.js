3738a81b8e973a0ee451933ef2a084616922bc8c
Eric Stratton <strattonericj@gmail.com>
2021-02-19 16:13:06 -0800

    working through selected keg test

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -3,38 +3,44 @@ import * as c from './../../actions/ActionTypes';
 
 describe("selectedKegReducer", () => {
 
-  let action;
-  const currentState = {
-    1: { name: 'Super Saison',
-    brand: 'Super Brews',
-    price: 8,
-    abv: 7,
-    stock: 124,
-    id: 1 },
-    2: { name: 'Serious Stout',
-    brand: 'Moose Beers',
-    price: 10,
-    abv: 12,
-    stock: 124,
-    id: 2 }
-  }
+  let action; // Do I actually need this?? // handleChangingSelectedKeg grabs keg id and then we'll pass it to our reducer.
+  // const currentState = {
+  //   1: { name: 'Super Saison',
+  //   brand: 'Super Brews',
+  //   price: 8,
+  //   abv: 7,
+  //   stock: 124,
+  //   id: 1 },
+  //   2: { name: 'Serious Stout',
+  //   brand: 'Moose Beers',
+  //   price: 10,
+  //   abv: 12,
+  //   stock: 124,
+  //   id: 2 }
+  // }
 
   test('Should return default state if no action passed into reducer', () => {
     expect(selectedKegReducer(null, { type: null })).toEqual(null);
   });
 
   test('Should select correct keg by ID', () => {
-    action = {
+    let keg = { name: 'Super Saison',
+      brand: 'Super Brews',
+      price: 8,
+      abv: 7,
+      stock: 124,
+      id: 1 
+      }
+      action = {
       type: c.SELECT_KEG,
-      id: 1
-    };
-    expect(selectedKegReducer(currentState, action)).toEqual({
-      1: { name: 'Super Saison',
-    brand: 'Super Brews',
-    price: 8,
-    abv: 7,
-    stock: 124,
-    id: 1 }
+      keg: keg }
+    expect(selectedKegReducer(null, action)).toEqual({
+    { name: 'Super Saison',
+        brand: 'Super Brews',
+        price: 8,
+        abv: 7,
+        stock: 124,
+        id: 1 }
     });
   });
 });
