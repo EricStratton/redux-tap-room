8611e92962d074631584d90c2610f091e1349e25
Eric Stratton <strattonericj@gmail.com>
2021-02-19 16:28:03 -0800

    remove comments

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -3,21 +3,7 @@ import * as c from './../../actions/ActionTypes';
 
 describe("selectedKegReducer", () => {
 
-  let action; // Do I actually need this?? // handleChangingSelectedKeg grabs keg id and then we'll pass it to our reducer.
-  // const currentState = {
-  //   1: { name: 'Super Saison',
-  //   brand: 'Super Brews',
-  //   price: 8,
-  //   abv: 7,
-  //   stock: 124,
-  //   id: 1 },
-  //   2: { name: 'Serious Stout',
-  //   brand: 'Moose Beers',
-  //   price: 10,
-  //   abv: 12,
-  //   stock: 124,
-  //   id: 2 }
-  // }
+  let action;
 
   test('Should return default state if no action passed into reducer', () => {
     expect(selectedKegReducer(null, { type: null })).toEqual(null);
