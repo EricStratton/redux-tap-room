a0dc5808e385826e25fe6a752e64ef5056e57d63
Eric Stratton <strattonericj@gmail.com>
2021-02-20 11:30:06 -0800

    fail first test

--- src/__tests__/reducers/edit-form-reducer.test.js
+++ src/__tests__/reducers/edit-form-reducer.test.js
@@ -1,2 +1,10 @@
 import editFormReducer from './../../reducers/edit-form-reducer';
-import * as c from './../../actions/ActionTypes';
+// import * as c from './../../actions/ActionTypes';
+
+describe("editFormReducer", () => {
+
+  test('Should return default state if no action is recognized', () => {
+    expect(editFormReducer(false, { type: null })).toEqual(false);
+  });
+
+});
