6277f0350b79069c5357b7f181bead40d36d6ea8
Eric Stratton <strattonericj@gmail.com>
2021-02-20 10:27:53 -0800

    fix error by passing selectedKegReducer to index reducer...

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import formVisibleReducer from './form-visible-reducer';
 import kegListReducer from './keg-list-reducer';
+import selectedKegReducer from './selected-keg-reducer';
 import { combineReducers } from 'redux';
+
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  mainKegList: kegListReducer
+  mainKegList: kegListReducer,
+  selectedKeg: selectedKegReducer
 });
 
 export default rootReducer;
