a35283bf8e88698ff3144cb014e210c754a1d8c1
Eric Stratton <strattonericj@gmail.com>
2021-02-20 11:37:29 -0800

    add editFormReducer to index reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,15 @@
 import formVisibleReducer from './form-visible-reducer';
 import kegListReducer from './keg-list-reducer';
 import selectedKegReducer from './selected-keg-reducer';
+import editFormReducer from './edit-form-reducer';
 import { combineReducers } from 'redux';
 
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
   mainKegList: kegListReducer,
-  selectedKeg: selectedKegReducer
+  selectedKeg: selectedKegReducer,
+  editing: editFormReducer
 });
 
 export default rootReducer;
