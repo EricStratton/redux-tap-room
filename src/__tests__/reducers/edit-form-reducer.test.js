447403a98509f64217d9606da3511625bc1a77cc
Eric Stratton <strattonericj@gmail.com>
2021-02-20 11:31:57 -0800

    fail second test

--- src/__tests__/reducers/edit-form-reducer.test.js
+++ src/__tests__/reducers/edit-form-reducer.test.js
@@ -1,10 +1,11 @@
 import editFormReducer from './../../reducers/edit-form-reducer';
-// import * as c from './../../actions/ActionTypes';
+import * as c from './../../actions/ActionTypes';
 
 describe("editFormReducer", () => {
-
   test('Should return default state if no action is recognized', () => {
     expect(editFormReducer(false, { type: null })).toEqual(false);
   });
-
+  test('Should toggle form visibility state to true', () => {
+    expect(editFormReducer(false, { type: c.TOGGLE_EDIT })).toEqual(true);
+  });
 });
